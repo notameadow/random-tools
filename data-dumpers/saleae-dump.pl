84f1da7022534d7cf38b16cdf08e390052ad6d6f
Meadow Ellis <git@meadow.ws>
2019-05-24 12:08:33 +0100

    SD: added handling UART dumps

--- data-dumpers/saleae-dump.pl
+++ data-dumpers/saleae-dump.pl
@@ -3,7 +3,9 @@
 =head1 saleae-dump.pl
 
 This script will take a .csv from Saleae and convert it to something useful -
-like a stream of hex values. Right now it assumes SPI.
+like a stream of hex values. Right now it recognises the data format by looking 
+at the first line of the file.
+The data is printed to stdout in hex as a string of [0-9A-F]{2}.
 
 =head2 SPI
 
@@ -43,7 +45,23 @@ as mentioned above, we will:
  - ignore MOSI after the first 0xFF
 
  This applies to both modes (we want MISO stream to be free of noise).
- 
+
+=head2 UART
+
+The format:
+Time [s],Value,Parity Error,Framing Error
+6.880548088000000,0x16,,
+6.881669188000000,0x73,,
+6.882801980000000,0x39,,
+
+The format is simple enough, so it's just a matter of merging the bytes into
+one string. We rely on Saleae to do its job.
+
+=head3 UART options
+
+--linebreak|lb, -l: insert newline *before* this string
+--breakline|bl, -b: insert newline *after* this string
+
 =cut
 
 use strict;
@@ -51,66 +69,105 @@ use warnings;
 use v5.014;
 use Getopt::Long;
 
-my $transactions;
-my @packets;
-my $packets;
+$| = 1;
+my $bl = 0;
+my $lb = 0;
 
-while (<>) {
-    chomp;
-    my ($ts, $packetId, $mosi, $miso) = split(/,/);
-    next unless ($packetId =~ /^\d+$/);
+GetOptions(
+    'linebreak|lb|l=s' => \$lb,
+    'breakline|bl|b=s' => \$bl,
+) or die "Incorrect options (-l)\n";
 
-    $mosi = oct($mosi);
-    $miso = oct($miso);
+my $header = <>;
+spi() if ($header =~ /MOSI/);
+uart() if ($header =~ /Parity Error,Framing Error/);
 
-    # We don't know if packets are always consecutive and none are missed
-    $packets->{$packetId} = 1;
+die "Header format not recognised: $header\n";
 
-    push (@{$transactions->{'raw'}->{$packetId}}, 
-        {mosi => $mosi, miso => $miso});
-}
-
-@packets = sort ({$a <=> $b} keys(%$packets));
-
-for my $packetId (@packets) {
-    push (@{$transactions->{'clear'}->{$packetId}}, 
-        {metaMosi => 1, packetId => $packetId});
-    my $mosiSend = 1;
-    for my $transaction (@{$transactions->{'raw'}->{$packetId}}) {
-        my $mosi = $transaction->{'mosi'};
-        my $miso = $transaction->{'miso'};
-
-        if ($mosiSend && ($mosi != 255)) {
-            $miso = 256;
-        } elsif ($mosiSend) {
-            push (@{$transactions->{'clear'}->{$packetId}}, 
-                {metaMiso => 1});
-            $mosiSend = 0;
-            $mosi = 256;
-        } else {
-            $mosi = 256;
-        }
-
-        push (@{$transactions->{'clear'}->{$packetId}}, 
-            {mosi => $mosi, miso => $miso});
-
+sub uart {
+    my $output;
+    while(<>) {
+        chomp;
+        my ($ts, $value, $parity, $framing) = split(/,/);
+        $value =~ s/0x(..)/$1/;
+        $output .= $value;
     }
+
+    if ($lb) {
+        $output =~ s/$lb/\n$lb/gm;
+    }
+
+    if ($bl) {
+        $output =~ s/$bl/$bl\n/gm;
+    }
+
+    say $output;
+    exit;
 }
 
-for my $packetId (@packets) {
-    for my $transaction (@{$transactions->{'clear'}->{$packetId}}) {
-        if ($transaction->{'metaMosi'}) {
-            print "Packet ID: $transaction->{'packetId'}\nMOSI:\n";
-        } elsif ($transaction->{'metaMiso'}) {
-            print "\nMISO:\n";
-        } else {
-            # At any moment either mosi or miso has to be 256
-            if ($transaction->{'mosi'} == 256) {
-                print sprintf('%02X', $transaction->{'miso'});
+sub spi {
+    my $transactions;
+    my @packets;
+    my $packets;
+
+    while (<>) {
+        chomp;
+        my ($ts, $packetId, $mosi, $miso) = split(/,/);
+        next unless ($packetId =~ /^\d+$/);
+
+        $mosi = oct($mosi);
+        $miso = oct($miso);
+
+        # We don't know if packets are always consecutive and none are missed
+        $packets->{$packetId} = 1;
+
+        push (@{$transactions->{'raw'}->{$packetId}}, 
+            {mosi => $mosi, miso => $miso});
+    }
+
+    @packets = sort ({$a <=> $b} keys(%$packets));
+
+    for my $packetId (@packets) {
+        push (@{$transactions->{'clear'}->{$packetId}}, 
+            {metaMosi => 1, packetId => $packetId});
+        my $mosiSend = 1;
+        for my $transaction (@{$transactions->{'raw'}->{$packetId}}) {
+            my $mosi = $transaction->{'mosi'};
+            my $miso = $transaction->{'miso'};
+
+            if ($mosiSend && ($mosi != 255)) {
+                $miso = 256;
+            } elsif ($mosiSend) {
+                push (@{$transactions->{'clear'}->{$packetId}}, 
+                    {metaMiso => 1});
+                $mosiSend = 0;
+                $mosi = 256;
             } else {
-                print sprintf('%02X', $transaction->{'mosi'});
+                $mosi = 256;
+            }
+
+            push (@{$transactions->{'clear'}->{$packetId}}, 
+                {mosi => $mosi, miso => $miso});
+
+        }
+    }
+
+    for my $packetId (@packets) {
+        for my $transaction (@{$transactions->{'clear'}->{$packetId}}) {
+            if ($transaction->{'metaMosi'}) {
+                print "Packet ID: $transaction->{'packetId'}\nMOSI:\n";
+            } elsif ($transaction->{'metaMiso'}) {
+                print "\nMISO:\n";
+            } else {
+                # At any moment either mosi or miso has to be 256
+                if ($transaction->{'mosi'} == 256) {
+                    print sprintf('%02X', $transaction->{'miso'});
+                } else {
+                    print sprintf('%02X', $transaction->{'mosi'});
+                }
             }
         }
+        print "\n";
     }
-    print "\n";
+    exit;
 }
